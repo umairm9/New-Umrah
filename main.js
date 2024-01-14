f9bec28c56609868ce534d49fb1812270ade97f0
royaluffb1317 <umbaloshi@gmail.com>
2024-01-14 23:44:28 +0500

    work

---
+++ main.js
@@ -0,0 +1,32 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav_links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line":"ri-menu-line")
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const bookingType = document.getElementById("booking-type");
+
+
+
+
+
+bookingType.addEventListener("click", (e) => {
+    Arrival.from(bookingType.getElementsByTagName("div")).forEach((item) => {
+        item.classList.remove("active");
+    });
+
+    e.target.classList.add("active");
+});
+
+
+
